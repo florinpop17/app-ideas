28c87c512b21b8091acdea6074fa6b45878052fc
LabyrinthLion <66320873+LabyrinthLion@users.noreply.github.com>
2021-10-28 11:19:44 +0100

    home page and 2/3s of Projects.csv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { Tablist, Tab, Pane, Link } from 'evergreen-ui';
+import React from 'react';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const tabs = ['Home', 'Tier 1', 'Tier 2', 'Tier 3'];
+    const pages = [<Home></Home>];
+    return (
+        <>
+            <Pane backgroundColor={selectedIndex === 0 ? '#FFDB14' : '#FAFBFF'}>
+                <Tablist
+                    flexBasis={240}
+                    padding={12}
+                    backgroundColor="#5863f8"
+                    display="flex"
+                    justifyContent="center"
+                >
+                    {tabs.map((tab, index) => (
+                        <Tab
+                            key={tab}
+                            id={tab}
+                            onSelect={() => setSelectedIndex(index)}
+                            isSelected={index === selectedIndex}
+                            aria-controls={`panel-${tab}`}
+                        >
+                            {tab}
+                        </Tab>
+                    ))}
+                    <Link href="https://github.com/florinpop17/app-ideas/">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1.75rem"
+                            height="1.75rem"
+                            viewBox="0 0 30 30"
+                            style={{ fill: '#000000' }}
+                        >
+                            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                        </svg>
+                    </Link>
+                </Tablist>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1440 320"
+                    height="3rem"
+                    width="100%"
+                    preserveAspectRatio="none"
+                    // style={{ position: 'absolute' }}
+                >
+                    <path
+                        fill="#5863f8"
+                        fill-opacity="1"
+                        d="M0,128L60,122.7C120,117,240,107,360,117.3C480,128,600,160,720,181.3C840,203,960,213,1080,192C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+                    ></path>
+                </svg>
+            </Pane>
+            <Pane background="tint1" flex="1" paddingBottom="2rem">
+                {pages.map((page, index) => (
+                    <Pane
+                        key={page}
+                        id={`panel-${page}`}
+                        role="tabpanel"
+                        aria-labelledby={page}
+                        aria-hidden={index !== selectedIndex}
+                        display={index === selectedIndex ? 'block' : 'none'}
+                    >
+                        {page}
+                    </Pane>
+                ))}
+            </Pane>
+        </>
+    );
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,267 @@
+import {
+    Pane,
+    Paragraph,
+    UnorderedList,
+    ListItem,
+    OrderedList,
+    Link,
+    Table,
+    Image,
+    Heading,
+} from 'evergreen-ui';
+import coverimage from './app-ideas.png';
+
+export default function Home() {
+    return (
+        <>
+            <Image width="100%" src={coverimage} alt="" />
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1440 320"
+                height="3rem"
+                width="100%"
+                preserveAspectRatio="none"
+                style={{ transform: 'translateY(-0.3rem)' }}
+            >
+                <path
+                    fill="#FFDB14"
+                    fill-opacity="1"
+                    d="M0,256L60,240C120,224,240,192,360,154.7C480,117,600,75,720,53.3C840,32,960,32,1080,42.7C1200,53,1320,75,1380,85.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+                ></path>
+            </svg>
+            <Pane margin="auto" width="90%">
+                <Paragraph>
+                    Have you ever wanted to build something but you had no idea
+                    what to do? Just as authors sometimes have "writers block"
+                    it's also true for developers. This list is intended to
+                    solve this issue once and for all! 👍
+                </Paragraph>
+                <Paragraph>These applications are:</Paragraph>
+                <UnorderedList>
+                    <ListItem>
+                        great for improving your coding skills 💪;
+                    </ListItem>
+                    <ListItem>
+                        great for experimenting with new technologies 🆕;
+                    </ListItem>
+                    <ListItem>
+                        great for adding to your portfolio to impress your next
+                        employer/client 📁;
+                    </ListItem>
+                    <ListItem>
+                        great for using as examples in tutorials (articles or
+                        videos) 📃;
+                    </ListItem>
+                    <ListItem>
+                        easy to complete and also easily extendable with new
+                        features 👌;
+                    </ListItem>
+                </UnorderedList>
+                <Paragraph>
+                    This is not just a simple list of projects, but a collection
+                    that describes each project in enough detail so that you can
+                    develop it from the ground up!
+                </Paragraph>
+                <Paragraph>
+                    Each project has the following <b>features:</b>
+                </Paragraph>
+                <OrderedList>
+                    <ListItem>A clear and descriptive objective;</ListItem>
+                    <ListItem>
+                        A list of User Stories which should be implemented
+                        (these stories act more as a guideline than a forced
+                        list of To-Do's. Feel free to add your own stories if
+                        you want);
+                    </ListItem>
+                    <ListItem>
+                        A list of bonus features that not only improve the base
+                        project, but also your skills at the same time (these
+                        are optional and should be attempted only after you have
+                        completed the required user stories)
+                    </ListItem>
+                    <ListItem>
+                        A list of bonus features that not only All the resources
+                        and links to help you find what you need to complete the
+                        project
+                    </ListItem>
+                </OrderedList>
+                <Paragraph>
+                    Here you can{' '}
+                    <Link
+                        href="https://www.youtube.com/watch?v=TNzCfgwIDCY"
+                        rel="nofollow"
+                    >
+                        watch a video about this repository
+                    </Link>
+                    .
+                </Paragraph>
+                <hr />
+                <Heading>Projects</Heading>
+                <Paragraph>
+                    Projects are divided into three tiers based on the knowledge
+                    and experience required to complete them.
+                </Paragraph>
+                <Table width="89%" margin="auto" marginY="1rem">
+                    <Table.Head>
+                        <Table.TextHeaderCell
+                            flexBasis="2rem"
+                            flexShrink={0}
+                            flexGrow={0}
+                        >
+                            Tier
+                        </Table.TextHeaderCell>
+                        <Table.TextHeaderCell>
+                            Developer Profile
+                        </Table.TextHeaderCell>
+                    </Table.Head>
+                    <Table.Body>
+                        <Table.Row height="fit-content" padding="0.5rem">
+                            <Table.Cell
+                                flexBasis="2rem"
+                                flexShrink={0}
+                                flexGrow={0}
+                            >
+                                1
+                            </Table.Cell>
+                            <Table.Cell>
+                                Developers in the early stages of their learning
+                                journey. Those who are typically focused on
+                                creating user-facing applications.
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row height="fit-content" padding="0.5rem">
+                            <Table.Cell
+                                flexBasis="2rem"
+                                flexShrink={0}
+                                flexGrow={0}
+                            >
+                                2
+                            </Table.Cell>
+                            <Table.Cell>
+                                Developers at an intermediate stage of learning
+                                and experience. They are comfortable in UI/UX,
+                                using development tools, and building apps that
+                                use API services.
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row height="fit-content" padding="0.5rem">
+                            <Table.Cell
+                                flexBasis="2rem"
+                                flexShrink={0}
+                                flexGrow={0}
+                            >
+                                3
+                            </Table.Cell>
+                            <Table.Cell>
+                                Developers who have all of the above, and are
+                                learning more advanced techniques like
+                                implementing backend applications and database
+                                services.
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+                <hr />
+                <Heading>Plans for the future</Heading>
+                <Paragraph>
+                    We are planning to add more and more projects to this
+                    repository. For this, we need your help! Find out how to
+                    contribute below. 👇
+                </Paragraph>
+                <Paragraph>
+                    We are also planning to create a website where you can
+                    easily browse through all of the projects.
+                </Paragraph>
+                <hr />
+                <Heading>Contribution</Heading>
+                <Paragraph>
+                    Any contributions are highly appreciated. 🙏 You can
+                    contribute in two ways:
+                </Paragraph>
+                <OrderedList>
+                    <ListItem>
+                        create an issue and tell us your idea 💡. Make sure that
+                        you use the new idea label in this case;
+                    </ListItem>
+                    <ListItem>
+                        fork the project and submit a PR with your new idea.
+                        Before doing that, please make sure that you read and
+                        follow the Contribution Guide;
+                    </ListItem>
+                </OrderedList>
+                <Heading size={400}>Examples</Heading>
+                <Paragraph>
+                    You can also add your own examples to the projects after you
+                    have completed them. I highly encourage you to do this as it
+                    will show others what amazing things were built! 👍
+                </Paragraph>
+                <hr />
+                <Heading>Spread the word!</Heading>
+                <Paragraph>
+                    If the information from this repo was useful to you in any
+                    way, make sure you give it a star 🌟, this way others can
+                    find it and benefit too! Together we can grow and make our
+                    community better! 😃
+                </Paragraph>
+                <Paragraph>
+                    Do you have any suggestions on how we could improve this
+                    project overall? Let us know! We'd love to hear your
+                    feedback!
+                </Paragraph>
+                <Heading size={400}>Main Contributors 🙂🙂</Heading>
+                <UnorderedList>
+                    <ListItem>
+                        <b>Florin Pop</b>:{' '}
+                        <Link
+                            href="https://twitter.com/florinpop1705"
+                            rel="nofollow"
+                        >
+                            Twitter
+                        </Link>{' '}
+                        -{' '}
+                        <Link
+                            href="https://youtube.com/florinpop"
+                            rel="nofollow"
+                        >
+                            YouTube Channel
+                        </Link>{' '}
+                        -{' '}
+                        <Link href="https://florin-pop.com" rel="nofollow">
+                            website
+                        </Link>
+                        .
+                    </ListItem>
+                    <ListItem>
+                        <b>Jim Medlock</b>:{' '}
+                        <Link
+                            href="https://twitter.com/jd_medlock"
+                            rel="nofollow"
+                        >
+                            Twitter
+                        </Link>{' '}
+                        -{' '}
+                        <Link
+                            href="https://medium.com/@jdmedlock"
+                            rel="nofollow"
+                        >
+                            articles
+                        </Link>
+                    </ListItem>
+                </UnorderedList>
+                <hr />
+                <Heading>100Days100Projects 🚀</Heading>
+                <Paragraph>
+                    As a bonus, I created the{' '}
+                    <Link
+                        href="https://www.florin-pop.com/blog/2019/09/100-days-100-projects"
+                        rel="nofollow"
+                    >
+                        #100Days100Projects challenge
+                    </Link>
+                    . You can find in there 100 more ideas of smaller projects.
+                    Enjoy! 😄
+                </Paragraph>
+            </Pane>
+        </>
+    );
+}
